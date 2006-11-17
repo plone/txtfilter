ccbe6e3317d45c6129a1cd928cdb161bf507a940
Whit Morriss <whit@openplans.org>
2006-11-17 22:02:48 +0000

    adding zope2 init
    svn path=/txtfilter/trunk/; revision=33865

---
+++ txtfilter/zope2.py
@@ -0,0 +1,27 @@
+from Extensions import Install
+del Install
+from Products.Archetypes import public as atapi
+from Products.CMFCore import CMFCorePermissions
+from Products.CMFCore import utils as cmf_utils
+from Products.CMFCore.DirectoryView import registerDirectory
+
+import api
+import config
+
+registerDirectory(config.SKINS_DIR, config.GLOBALS)
+
+def initialize(context):
+    if config.INSTALL_TYPES:
+        import example
+    
+        types = atapi.listTypes(config.PROJECTNAME)
+
+        content_types, constructors, ftis = atapi.process_types( types,
+                                                                 config.PROJECTNAME)
+        cmf_utils.ContentInit(
+            config.PROJECTNAME + ' Example Content',
+            content_types      = content_types,
+            permission         = CMFCorePermissions.AddPortalContent,
+            extra_constructors = constructors,
+            fti                = ftis,
+            ).initialize(context)
